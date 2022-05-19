e20fec35a80d37c40442e44e794b2cbeff1a1ca0
Sam-Guliker <samguliker@hotmail.com>
2022-05-19 06:58:54 +0200

    update

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 import sanityClient from "@sanity/client";
 
-export default sanityClient({
+export const client = sanityClient({
   projectId: process.env.sanityId, // find this at manage.sanity.io or in your sanity.json
   dataset: "production", // this is from those question during 'sanity init'
   useCdn: true,
